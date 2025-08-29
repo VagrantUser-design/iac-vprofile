5e8167dfd397eea888201207f93c06e41d44f296
vishnu <pasunutivishnu1523@gmail.com>
2025-08-29 22:44:14 +0530

    test-workflow

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,13 +27,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "myworkflow8309"
+    bucket = "automationiac"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
 
   required_version = "~> 1.6.3"
 }
-##
-##
-##
